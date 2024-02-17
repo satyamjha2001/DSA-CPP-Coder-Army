c2575faadae4a319fb246091270d19b8fd3d8204
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-02-17 23:29:12 +0530

    first commit

---
+++ 3.Introduction to C++/1productOfTwoNum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a,b;
+    cout<<"Enter two numbers ";
+    cin>>a>>b;
+    cout<<"Product of two numbers is "<<a*b;
+    return 0;
+}
---
+++ 3.Introduction to C++/2SubOfTwoNum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a,b;
+    cout<<"Enter two numbers ";
+    cin>>a>>b;
+    cout<<"Subtraction of two numbers is "<<a-b;
+    return 0;
+}
